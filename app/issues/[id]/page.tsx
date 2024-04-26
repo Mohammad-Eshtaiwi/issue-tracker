c1a6068c576a47cc13af94a26eec26f3b051938e
LetMeGuideU <53796803+LetMeGuideU@users.noreply.github.com>
2024-04-26 23:53:32 +0300

    Add the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import Markdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 interface Props {
   params: { id: string };
@@ -15,16 +17,30 @@ const IssueDetailsPage = async ({ params }: Props) => {
   if (!issue) return notFound();
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex className="gap-3 items-center my-3">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose mt-3">
-        <Markdown>{issue.description}</Markdown>
-      </Card>
-    </div>
+    <Grid
+      columns={{
+        initial: "1",
+        md: "2",
+      }}
+      gap="5"
+    >
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex className="gap-3 items-center my-3">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose mt-3">
+          <Markdown>{issue.description}</Markdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+          <Pencil2Icon />
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
